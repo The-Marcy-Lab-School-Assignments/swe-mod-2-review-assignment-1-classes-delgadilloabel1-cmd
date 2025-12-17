0f3d09dcf24a4763a734bb08f2c75cce6bbb9c25
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2025-12-17 07:20:47 -0500

    completed mediaItem

--- src/1-media.js
+++ src/1-media.js
@@ -1,24 +1,28 @@
 // Problem 1: Inheritance and Polymorphism - Media Player
 // Implement your MediaItem, Song, Podcast, and Audiobook classes below
 class MediaItem {
-
+  constructor(title, duration) {
+    this.title = title;
+    this.duration = duration;
+  }
+  play() {
+    return `Playing: ${this.title}`;
+  }
+  getFormattedDuration() {
+    let mins = Math.floor(this.duration / 60);
+    let secs = this.duration % 60;
+    // let formattedMins = String(mins).padStart(2, `0`);
+    let formattedSecs = String(secs).padStart(2, `0`);
+    return `${mins}:${formattedSecs}`;
+  }
 }
 
-class Song {
+class Song {}
 
-}
+class Podcast {}
 
-class Podcast {
+class Audiobook {}
 
-}
-
-class Audiobook {
-
-}
-
-const test = () => {
-
-}
+const test = () => {};
 
 module.exports = { MediaItem, Song, Podcast, Audiobook };
-
